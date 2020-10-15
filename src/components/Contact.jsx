211ad86f4eef6682b8e0f4ba212e1c7c3ef1e405
dianecasanova <festusnova1989@gmail.com>
2020-10-15 12:32:15 +0200

    Contact transform class, state initialized in the constructor, click online & offline

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import "./Contact.css";
-import PropTypes from "prop-types";
 
-function Contact(props) {
-  return (
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: false,
+    };
+    this.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    const newStatus = !this.state.online;
+    this.setState({
+      online: newStatus,
+    });
+  }
+
+  render() {
+    return (
     <div className="Contact">
-      <img className="avatar" src={props.avatar} alt={props.name} />
+      <img className="avatar" src={this.props.avatar} alt={this.props.name} />
       <div>
-        <p className="name">{props.name}</p>
-        <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"} />
-          <p className="status-text">{props.online ? "Online" : "Offline"}</p>
+        <p className="name">{this.props.name}</p>
+        <div className="status" onClick={this.increment} >
+          <div className={this.state.online ? "status-online" : "status-offline"} />
+          <p className="status-text">{this.state.online ? "Online" : "Offline"}</p>
         </div>
       </div>
     </div>
-  );
+    );
+  }
 }
 
-Contact.propTypes = {
-  name: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  online: PropTypes.bool
-};
 
 export default Contact;
